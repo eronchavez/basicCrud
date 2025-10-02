0f0c901337b1bc9d14e61a393f9b0172a33a5879
Ron chavez <aaronchavez.dev@gmail.com>
2025-10-02 13:01:21 +0800

    added error message

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,12 +1,20 @@
-    
-    
-   
+ 
+      
+        <input type="text" name="title" placeholder="TITLE">
+        @error('title')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
 
+      
+        <input type="text" name="author" placeholder="AUTHOR">
+        @error('author')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
 
-        <input type="text" name="title" placeholder="Title"><br>
+     
+        <input type="text" name="published_year" placeholder="YEAR">
+        @error('published_year')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
 
-        <input type="text" name="author" placeholder="Author"><br>
-
-        <input type="text" name="published_year" placeholder="Published Year"><br>
-
-        <input type="submit" value="Save">
+        <br><br>
